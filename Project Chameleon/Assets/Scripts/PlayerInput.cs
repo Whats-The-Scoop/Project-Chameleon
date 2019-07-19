c87c9a8af16a0bb6e4bf12b221d9d2af7788773d
Whats-The-Scoop <53028581+Whats-The-Scoop@users.noreply.github.com>
2019-07-19 06:34:33 -0700

    Create PlayerInput.cs

---
+++ Project Chameleon/Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using UnityEngine;
+
+[RequireComponent (typeof (Player))]
+public class PlayerInput : MonoBehaviour
+{
+    Player player;
+    void Start()
+    {
+        player = GetComponent<Player>();
+    }
+
+    void Update()
+    {
+        Vector2 directionInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+       player.SetDirectionalImput(directionInput);
+
+        if (Input.GetKeyDown (KeyCode.Space))
+        {
+            player.OnJumpInputDown();
+        }
+        if (Input.GetKeyUp (KeyCode.Space))
+        {
+            player.OnJumpInputUp();
+        }
+    }
+}
